b96855696a4c1eba71512a8f3fa066cb46a28034
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-01-26 19:31:23 -0300

    Added equals and Hasjcode

--- src/main/java/com/deivcode/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/deivcode/dscommerce/entities/OrderItemPK.java
@@ -5,6 +5,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
 
@@ -34,5 +36,20 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
     }
 }
